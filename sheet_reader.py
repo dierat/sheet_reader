220a9896759b6e6da3f1c1bdfc4ac98f706ca670
dierat <diedra.rater@gmail.com>
2015-03-27 20:00:01 -0700

    adds filter to remove excess quotations and limits splits so the description doesn't get segmented

--- sheet_reader.py
+++ sheet_reader.py
@@ -7,9 +7,20 @@ lines = f1.readlines()
 f2.write('chars = [')
 
 for i in range(len(lines)):
-	temp_array = lines[i].split(',')
 	temp_string = '['
+	temp_array = lines[i].split(',', 5)
 	for j in range(len(temp_array)):
+		if j == 5:
+			descrip = temp_array[j]
+			edited_descrip = ''
+			for k in range(len(descrip)):
+			 	if k != 0 and k != (len(descrip) - 2):
+			 		if descrip[k] != '"':
+			 			edited_descrip += descrip[k]
+			 		if descrip[k] == '"':
+			 			if descrip[k + 1] != '"':
+			 				edited_descrip += descrip[k]
+			temp_array[j] = edited_descrip
 		temp_string = temp_string + "'" + temp_array[j] + "'"
 		if j != len(temp_array) - 1:
 			temp_string = temp_string + ','

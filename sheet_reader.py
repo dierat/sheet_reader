97287752a6e110fdf6e6ef611e507556a011757a
dierat <diedra.rater@gmail.com>
2015-03-27 18:38:47 -0700

    adds quotes around array items before writing to second file

--- sheet_reader.py
+++ sheet_reader.py
@@ -1,11 +1,18 @@
 f1 = open('101_radicals.csv', 'r')
 f2 = open('test2.txt', 'w')
 
+
 lines = f1.readlines()
 
-for i in range(len(lines)):
-	if i % 2 == 0:
-		f2.write(lines[i])
+for line in lines:
+	temp_array = line.split(',')
+	temp_string = ''
+	for i in range(len(temp_array)):
+		temp_string = temp_string + "'" + temp_array[i] + "'"
+		if i != len(temp_array) - 1:
+			temp_string = temp_string + ','
+	f2.write(temp_string)
+		
 
 f1.close()
 f2.close()

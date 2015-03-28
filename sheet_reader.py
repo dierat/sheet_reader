42aa8d1f96cef2220d8601bd3e872b2843a36269
dierat <diedra.rater@gmail.com>
2015-03-27 18:10:56 -0700

    changes test code to use 101_radicals.csv and only write every other line to test2.txt

--- sheet_reader.py
+++ sheet_reader.py
@@ -1,10 +1,11 @@
-f1 = open('test1.txt', 'r')
+f1 = open('101_radicals.csv', 'r')
 f2 = open('test2.txt', 'w')
 
 lines = f1.readlines()
 
-for line in lines:
-	f2.write(line)
+for i in range(len(lines)):
+	if i % 2 == 0:
+		f2.write(lines[i])
 
 f1.close()
 f2.close()

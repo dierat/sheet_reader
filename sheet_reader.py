eb8985d8eda526a3adeeab8b0a88cb4f20134c13
dierat <diedra.rater@gmail.com>
2015-03-27 18:54:27 -0700

    adds commas between arrays

--- sheet_reader.py
+++ sheet_reader.py
@@ -6,14 +6,16 @@ lines = f1.readlines()
 
 f2.write('chars = [')
 
-for line in lines:
-	temp_array = line.split(',')
+for i in range(len(lines)):
+	temp_array = lines[i].split(',')
 	temp_string = '['
-	for i in range(len(temp_array)):
-		temp_string = temp_string + "'" + temp_array[i] + "'"
-		if i != len(temp_array) - 1:
+	for j in range(len(temp_array)):
+		temp_string = temp_string + "'" + temp_array[j] + "'"
+		if j != len(temp_array) - 1:
 			temp_string = temp_string + ','
 	temp_string = temp_string + ']'
+	if i != len(lines) - 1:
+		temp_string = temp_string + ','
 	f2.write(temp_string)
 
 f2.write(']')
